12d06e101094330fe903d14ab65310377e504419
shafayet16 <hirasan893@gmail.com>
2026-09-08 20:58:13 +0600

    fix: meta data and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import NavbarWrapper from "./components/NavbarWrapper";
 import CartDrawer from "./components/CartDrawer";
 import { CartProvider } from "./context/CartContext"; // Adjust to "@/context/CartContext" if context folder is in project root
 import "./globals.css";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'RASTA',
+  description: 'Explore the Rasta collection.',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
